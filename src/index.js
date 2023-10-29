50a8c207ed5454796f9b861d1338b1200d72cebc
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-10-30 00:24:57 +0200

    add

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="https://ihorhodebskyi.github.io/test-task">
+    <BrowserRouter basename="/test-task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
